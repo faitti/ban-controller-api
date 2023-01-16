73345964d742a5050f3d2551539f0cd7ec479745
faitti <aleksanteri.nissinen@jyno.fi>
2023-01-16 15:43:28 +0200

    feat: base app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, http::StatusCode, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn root() -> impl Responder {
+    StatusCode::FORBIDDEN
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(root)
+    })
+    .bind(("127.0.0.1", "8080"))?
+    .run()
+    .await
 }
